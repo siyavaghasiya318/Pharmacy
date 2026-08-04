5badebe159cca3c9b32f5f548260da71c0997358
siyavaghasiya318 <siyavaghasiya318@gmail.com>
2026-08-04 15:44:19 +0530

    Refactor GenerateUserToken function for improved readability and update cookie settings

--- Backend/Token/UserToken.js
+++ Backend/Token/UserToken.js
@@ -1,20 +1,20 @@
 import jwt from "jsonwebtoken"
 
-export const GenerateUserToken = async(userid,res) => {
+export const GenerateUserToken = async (userid, res) => {
     try {
-        const token = jwt.sign({userid}, process.env.JWT_SECRET, {expiresIn:"2d"})
+        const token = jwt.sign({ userid }, process.env.JWT_SECRET, { expiresIn: "2d" })
 
         res.cookie("token", token, {
             httpOnly: true,
-            sameSite: "lax",
-            secure: process.env.NODE_ENV !== "development",
-            maxAge: 2 * 24 * 60 * 60 * 1000 
-         })
+            secure: true,
+            sameSite: "none",
+            maxAge: 2 * 24 * 60 * 60 * 1000,
+        });
 
-         
-         return token
+
+        return token
     } catch (error) {
         console.log("GenerateUserToken error", error);
-        
+
     }
 } 
